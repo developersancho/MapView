e9ac427133040857ca1695a83962728e40f3384a
developersancho <developersancho@gmail.com>
2018-04-22 17:46:30 +0300

    ---
    ---

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,CLLocationManagerDelegate {
+    @IBOutlet weak var harita: MKMapView!
+    @IBOutlet weak var goruntuSecici: UISegmentedControl!
+    var haritaYoneticisi = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let isaretlenmisYer: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 41.0102645, longitude: 28.9786778)
+        
+        let aciklama = MKPointAnnotation()
+        aciklama.coordinate = isaretlenmisYer
+        aciklama.title = "İSTANBUL MERKEZ"
+        aciklama.subtitle = "İstanbul,Türkiye"
+        self.harita.addAnnotation(aciklama)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +32,44 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func goruntuSec(_ sender: Any) {
+        switch goruntuSecici.selectedSegmentIndex {
+        case 0:
+            harita.mapType = MKMapType.standard
+            break
+        case 1:
+            harita.mapType = MKMapType.satellite
+            break
+        case 2:
+            harita.mapType = MKMapType.hybrid
+            break
+        default:
+            break
+        }
+    }
+    
+    @IBAction func yerimiBul(_ sender: Any) {
+        haritaYoneticisi.delegate = self
+        haritaYoneticisi.desiredAccuracy = kCLLocationAccuracyBest
+        haritaYoneticisi.requestWhenInUseAuthorization()
+        haritaYoneticisi.startUpdatingLocation()
+        harita.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLoc : CLLocation = locations[0] as CLLocation
+        haritaYoneticisi.stopUpdatingLocation()
+        let alan = CLLocationCoordinate2D(latitude: userLoc.coordinate.latitude, longitude: userLoc.coordinate.longitude)
+        let aralik = MKCoordinateSpanMake(0.8, 0.8)
+        let bolge = MKCoordinateRegion(center: alan, span: aralik)
+        harita.setRegion(bolge, animated: true)
+    }
+    
+    
+    @IBAction func isaretliAlanaGotur(_ sender: Any) {
+        let baglanti = URL(string: "http://maps.apple.com/maps?daddr=41.0102645,28.9786778")
+        UIApplication.shared.openURL(baglanti!)
+    }
+    
 }
 
